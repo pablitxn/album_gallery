03eb86b89327b5c9db6518c87e580dc1ac9b711c
Pablitvs <correo@pablocoronel.com>
2019-06-23 06:20:10 -0300

    agregando el componente y sus estilos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+import Gallery from './components/album_gallery'
+
 
 ReactDOM.render(
-  <App />,
+  <Gallery />,
   document.getElementById('root')
 );
